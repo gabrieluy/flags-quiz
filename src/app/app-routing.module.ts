4ba09f4e2e5fa4cc959d136a36904a5f077c82ff
Gabriel <gabrieluy@gmail.com>
2023-01-13 19:00:16 -0300

    Simplify routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GameComponent } from './game/game.component';
-import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'game', component: GameComponent },
-  { path: '', redirectTo: '/game', pathMatch: 'full' },
-];
+const routes: Routes = [{ path: '', pathMatch: 'full', component: GameComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
