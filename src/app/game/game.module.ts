222cf4cde9a14437cb265f88e5fc428c67962e1c
Gabriel Nuñez <gabirluy@gmail.com>
2023-01-13 19:01:48 -0300

    Improve game info

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -5,9 +5,11 @@ import { MessageModule } from 'primeng/message';
 import { MessageService } from 'primeng/api';
 import { CommonModule } from '@angular/common';
 import { FlagGridComponent } from '../ui/flag-grid/flag-grid.component';
+import { InfoBtnComponent } from '../ui/info-btn/info-btn.component';
+import { TagModule } from 'primeng/tag';
 
 @NgModule({
-  imports: [CommonModule, ButtonModule, MessageModule, FlagGridComponent],
+  imports: [CommonModule, ButtonModule, MessageModule, FlagGridComponent, InfoBtnComponent, TagModule],
   declarations: [GameComponent],
   exports: [GameComponent],
   providers: [MessageService],
