b246428c41a71d6cad1d34556602231277092829
Gabriel Nuñez <gabirluy@gmail.com>
2023-04-18 20:45:43 -0300

    Add floating back button

--- src/app/game/pipes/progress-percentage.pipe.ts
+++ src/app/game/pipes/progress-percentage.pipe.ts
@@ -6,9 +6,7 @@ import { GameStatus } from '../interfaces/game-status.interface';
 })
 export class ProgressPercentagePipe implements PipeTransform {
   transform(status: GameStatus): number {
-    console.log(`actual = ${status.actualFlagCount} total: ${status.actualFlagCount + status.remainingFlags}`);
     const value = Math.floor((status.actualFlagCount * 100) / (status.actualFlagCount + status.remainingFlags));
-    console.log(`valor = ${value}`);
     return value;
   }
 }
