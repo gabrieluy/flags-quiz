d92043a7eed4f3f9cac44cb0ff1efd4f1ea7611d
Gabriel <gabrieluy@gmail.com>
2023-06-18 21:49:36 +0200

    add pagination to answer history

--- src/app/game/answer-history/answer-history.component.ts
+++ src/app/game/answer-history/answer-history.component.ts
@@ -1,5 +1,5 @@
 import { transition, trigger, useAnimation } from '@angular/animations';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges } from '@angular/core';
 import { Answer } from 'src/app/game/interfaces/game-status.interface';
 import { slideR } from 'src/app/ui/animations/slideR.animation';
 
@@ -9,7 +9,7 @@ import { slideR } from 'src/app/ui/animations/slideR.animation';
   animations: [trigger('slideR', [transition(':enter', [useAnimation(slideR, { params: { time: '0.2s' } })])])],
   template: `
     <div class="grid">
-      <div *ngFor="let answer of history" class="col-12 md:col-6 lg:col-4">
+      <div *ngFor="let answer of visibleHistory" class="col-12 md:col-6 lg:col-4">
         <fq-card @slideR>
           <div class="flex justify-content-between">
             <fq-flag-img [flag]="answer.country.cca2" class="align-self-start w-7rem"></fq-flag-img>
@@ -19,8 +19,33 @@ import { slideR } from 'src/app/ui/animations/slideR.animation';
         </fq-card>
       </div>
     </div>
+    <div *ngIf="visibleBtn" class="text-center my-3">
+      <button
+        pButton
+        label="Show More"
+        (click)="showMoreItems()"
+        class="p-button-primary p-button-outlined bg-white-alpha-90"></button>
+    </div>
   `,
 })
-export class AnswerHistoryComponent {
+export class AnswerHistoryComponent implements OnChanges {
   @Input() history: Answer[] = [];
+
+  public visibleHistory: Answer[] = [];
+  public visibleBtn = false;
+  public visibleHistorySize = 6;
+
+  public ngOnChanges(): void {
+    this.updateVisibleHistory();
+  }
+
+  public updateVisibleHistory(): void {
+    this.visibleHistory = this.history.slice(0, this.visibleHistorySize);
+    this.visibleBtn = this.history.length > this.visibleHistory.length;
+  }
+
+  showMoreItems(): void {
+    this.visibleHistorySize += 3;
+    this.updateVisibleHistory();
+  }
 }
