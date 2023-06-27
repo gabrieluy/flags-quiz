e89c287f5bb3e89a310b7ce1b9bdef9665d5cf4f
Gabriel <gabrieluy@gmail.com>
2023-06-27 19:17:34 +0200

    refactor: change tittle style

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,12 +5,11 @@ import { HomeComponent } from './home.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { CardComponent } from '../ui/card/card.component';
 import { TranslocoModule } from '@ngneat/transloco';
-import { TitleComponent } from '../ui/title/title.component';
 
 @NgModule({
   declarations: [HomeComponent],
   exports: [HomeComponent],
   providers: [],
-  imports: [CardComponent, TitleComponent, CommonModule, AppRoutingModule, ButtonModule, TranslocoModule],
+  imports: [CardComponent, CommonModule, AppRoutingModule, ButtonModule, TranslocoModule],
 })
 export class HomeModule {}
