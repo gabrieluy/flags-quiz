819af9d06dc85cc0cfaec7f1cafefaa2812e7895
Gabriel <gabrieluy@gmail.com>
2023-07-07 16:54:09 +0200

    feat: create src pipe

--- src/app/ui/flag-img/flag-img.component.ts
+++ src/app/ui/flag-img/flag-img.component.ts
@@ -1,16 +1,12 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { FlagSrcPipe } from 'src/app/game/pipes/flag-src.pipe';
 
 @Component({
   selector: 'fq-flag-img',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: `
-    <img
-      class="border-round shadow-2"
-      [class]="class"
-      [src]="'assets/flags/4x3/' + flag.toLowerCase() + '.svg'"
-      (load)="onImageLoad()" />
-  `,
+  template: ` <img class="border-round shadow-2" [class]="class" [src]="flag | flagSrc" (load)="onImageLoad()" /> `,
+  imports: [FlagSrcPipe],
 })
 export class FlagImgComponent {
   @Input() flag!: string;
