c60059185ec38fe779b56c211b3758eeaac8940c
Gabriel Nuñez <gabirluy@gmail.com>
2023-07-07 16:43:12 +0200

    feat: change app name

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,7 @@ import { GameManagerService } from '../game/game-manager.service';
       <fq-card *transloco="let t; read: 'home'">
         <div class="flex flex-row flex-wrap">
           <div class="flex justify-content-center col-12">
-            <h1>Flags Quiz!</h1>
+            <h1>Flags Mastery!</h1>
           </div>
           <div class="flex justify-content-center col-12">
             <img class="w-10rem h-10rem" src="assets/images/pet.png" />
